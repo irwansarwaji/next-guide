1bc99e1d8dd5529fe758cb8e7b07e0493e989b8f
Irwan Sarwaji <irwan.sarwaji@hva.nl>
2022-11-23 16:07:19 +0000

    use next image component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["rickandmortyapi.com"],
+    loader: "custom", // we made a custom loader
+    path: "/",
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
