9ae93cb1f182731e044ace029f04d95b8a578d33
Irwan Sarwaji <irwan.sarwaji@hva.nl>
2022-11-25 19:33:31 +0000

    usage of css

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import imageLoader from "../../imageLoader";
 import { Character, GetCharacterResults } from "../../types";
+import styles from "../../styles/Character.module.css";
 
 function CharacterPage({ character }: { character: Character }) {
   const router = useRouter(); // for if you want to check character id import useRouter. You can see it on the browser and server terminal
 
   console.log(router.query);
   return (
-    <div key={character.id}>
+    <div className={styles.container} key={character.id}>
       <h1>{character.name}</h1>
       <Image
         loader={imageLoader}
